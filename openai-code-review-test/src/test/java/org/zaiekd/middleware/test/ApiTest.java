19a91274a432242c6f4e552668274aca0ec6b175
Lihan Zou <zoulh9187@outlook.com>
2025-02-13 15:24:33 -0600

    feat: v2

--- openai-code-review-test/src/test/java/org/zaiekd/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/org/zaiekd/middleware/test/ApiTest.java
@@ -19,6 +19,8 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("aaa"));
+        System.out.println(Integer.parseInt("aaa1"));
+        System.out.println(Integer.parseInt("aaa2"));
+        System.out.println(Integer.parseInt("aaa3"));
     }
 }
