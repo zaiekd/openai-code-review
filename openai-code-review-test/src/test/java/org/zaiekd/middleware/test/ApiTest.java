1ad06e6383f2244c281cb62cf5543ab9008145cf
Lihan Zou <zoulh9187@outlook.com>
2025-02-15 16:45:43 -0600

    feat: 评审日志写入文件 apitest v5.1

--- openai-code-review-test/src/test/java/org/zaiekd/middleware/test/ApiTest.java
+++ openai-code-review-test/src/test/java/org/zaiekd/middleware/test/ApiTest.java
@@ -20,6 +20,5 @@ public class ApiTest {
     @Test
     public void test() {
         System.out.println(Integer.parseInt("aaa1234"));
-        System.out.println(Integer.parseInt("aaa123"));
     }
 }
